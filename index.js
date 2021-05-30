827789a167dffdc028d82ea56905ad0f03aa4c78
elifatesalp <59237936+elifatesalp@users.noreply.github.com>
2021-05-31 00:00:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const bitirButton = document.getElementById('bitir-btn')
+const gonderButton = document.getElementById('gonder-btn')
+const tamamButton = document.getElementById('tamam-btn')
+const aramaButton = document.getElemenyById ('arama-btn')
+
+
+bitirButton.addEventListener('click', anketBitir)
+gonderButton.addEventListener('click', formGonder)
+tamamButton.addEventListener('click', gorusGonder)
+aramaButton.addEventListener('clik', aramaYap)
 
