9f01fe8814223ae8c5b7aba14fdd89a1c17687ad
elifatesalp <59237936+elifatesalp@users.noreply.github.com>
2021-05-31 00:01:24 +0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,16 @@
+<html>
+       <?php
+
+
+if (isset($_POST['submit'])){
+    print_r($_POST);
+
+    echo "MESAJINIZ BAŞARIYLA GÖNDERİLMİŞTİR! BOL ŞANSLAR DİLERİZ!";
+}
+else
+{
+    echo "MESAJINIZ GÖNDERİLEMEDİ! LÜTFEN TEKRAR DENEYİNİZ.";
+}
+   ?>
+   
+   </html>   
