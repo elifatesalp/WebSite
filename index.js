1cd85e9188d942b6f008c6562f56a4bd09b3b236
elifatesalp <59237936+elifatesalp@users.noreply.github.com>
2021-06-02 00:01:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,32 @@ gonderButton.addEventListener('click', formGonder)
 tamamButton.addEventListener('click', gorusGonder)
 aramaButton.addEventListener('clik', aramaYap)
 
+
+function video() 
+{
+    return !!document.createElement('video').canPlayType; 
+}
+    if ( !video() ) 
+       {
+          videoControls.style.display = 'none';
+       }
+
+function epostaDogrula(theform) 
+{
+    if (/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/.test(theform.email-id.value))
+    {
+     return(true);
+    }
+    alert("Geçersiz e-posta adresi! Lütfen tekrar deneyiniz!");
+     return(false);
+ }
+ 
+ function showDate() 
+ {
+    var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1; 
+    var curr_year = d.getFullYear();
+    document.write(curr_date + "-" + curr_month + "-" + curr_year);
+  }
+
