fd3b78709cb667c0d8415c23f21ad71be988ab89
elifatesalp <59237936+elifatesalp@users.noreply.github.com>
2021-06-04 17:23:16 +0300

    Update gorus.php

--- gorus.php
+++ gorus.php
@@ -1,12 +1,18 @@
+<!DOCTYPE html>
 <html>
-       <?php
+<body>
+    
 
-
+<?php
 if (isset($_POST['submit'])){
     print_r($_POST);
 
     echo "GÖRÜŞLERİNİZ BAŞARIYLA GÖNDERİLMİŞTİR! TEŞEKKÜR EDERİZ!";
 }
+else
+{
+    echo "GÖRÜŞLERİNİZ GÖNDERİLEMEDİ! LÜTFEN TEKRAR DENEYİNİZ.";
+}
    ?>
-   
+</body>
    </html>   
