d9ed9437b280adba6424d3dd581f3dcf2e79a6ac
elifatesalp <59237936+elifatesalp@users.noreply.github.com>
2021-06-14 19:19:26 +0300

    Update testsql.php

--- testsql.php
+++ testsql.php
@@ -1,3 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
 <?php
 
 $sunucu = "localhost";
@@ -8,13 +12,15 @@ $port = NULL;
 $mysqli = new mysqli('127.0.0.1', $sunucu, $user, $password, $database, $port);
 
 if ($mysqli->connect_error) {
+
     die('Connect Error (' . $mysqli->connect_errno . ') '
+
             . $mysqli->connect_error);
 }
 echo '<p>Connection OK '. $mysqli->host_info.'</p>';
-echo '<p>Server '.$mysqli->server_info.'</p>';
-echo '<p>Initial charset: '.$mysqli->character_set_name().'</p>';
 
 $mysqli->close();
 ?>
+</body>
+   </html>   
 
