01550a932a65ed5b9c9562d8befac4ee358f70c8
elifatesalp <59237936+elifatesalp@users.noreply.github.com>
2021-06-17 21:34:41 +0300

    Update form.php

--- form.php
+++ form.php
@@ -3,14 +3,19 @@
 <body>
 
 <?php
-if (isset($_POST['submit'])){
+
+if (isset($_POST['submit']))
+{
    
     echo "MESAJINIZ GÖNDERİLEMEDİ! LÜTFEN TEKRAR DENEYİNİZ";
 }
+
 else
 {
+    
     echo "MESAJINIZ BAŞARIYLA GÖNDERİLMİŞTİR! BOL ŞANSLAR DİLERİZ!";
 }
+
    ?>
 </body>
    </html>   
