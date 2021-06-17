3af5c7f70256ca5750d6e445110b4c8924f2bc88
elifatesalp <59237936+elifatesalp@users.noreply.github.com>
2021-06-17 23:18:01 +0300

    Create anket.php

---
+++ anket.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+<?php
+
+if (isset($_POST['submit']))
+{
+
+    echo "ANKETİNİZ GÖNDERİLEMEDİ! LÜTFEN TEKRAR DENEYİNİZ.";
+}
+else
+{
+
+    echo "ANKETİMİZİ DOLDURDUĞUNUZ İÇİN TEŞEKKÜR EDERİZ! İLETİŞİM FORMUNU DA DOLDURARAK HEDİYE KAMPANYAMIZDAN YARARLANABİLİRSİNİZ!";
+
+}
+
+   ?>
+</body>
+   </html>   
