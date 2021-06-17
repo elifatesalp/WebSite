d113801bbbda897bf90ddb53f267f4d6ec89946e
elifatesalp <59237936+elifatesalp@users.noreply.github.com>
2021-06-17 22:03:56 +0300

    Update testsql.php

--- testsql.php
+++ testsql.php
@@ -9,15 +9,18 @@ $user = 'root';
 $password = ''; 
 $database = 'arama'; 
 $port = NULL; 
+
 $mysqli = new mysqli('127.0.0.1', $sunucu, $user, $password, $database, $port);
 
-if ($mysqli->connect_error) {
+if ($mysqli->connect_error) 
+{
 
-    die('Connect Error (' . $mysqli->connect_errno . ') '
+    die('Bağlantı Başarısız (' . $mysqli->connect_errno . ') '
 
             . $mysqli->connect_error);
 }
-echo '<p>Connection OK '. $mysqli->host_info.'</p>';
+
+echo '<p>Bağlantı Başarılı '. $mysqli->host_info.'</p>';
 
 $mysqli->close();
 ?>
